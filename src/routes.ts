6096333309e24618cbaff5a60f24503d34f5f7d7
Jailson Sousa Bastos <jailsonsousa@MacBook-Pro-de-Jailson.local>
2021-10-20 11:49:53 -0300

    change in api path

--- src/routes.ts
+++ src/routes.ts
@@ -6,9 +6,9 @@ import sessionsRouter from './modules/users/infra/http/routes/sessions.routes';
 import citizensRouter from './modules/citizen/infra/http/routes/citizens.routes';
 
 const routes = Router();
-routes.use('/users', usersRouter);
-routes.use('/sessions', sessionsRouter);
+routes.use('/api/users', usersRouter);
+routes.use('/api/sessions', sessionsRouter);
 
-routes.use('/citizens', citizensRouter);
+routes.use('/api/citizens', citizensRouter);
 
 export default routes;
